694ec615155bf08f16869b50361d4f67bca4f246
binfangHUANG <bfhuang@thoughtworks.com>
2021-01-15 17:38:48 +0800

    [Binfang] add the script to check container status

---
+++ check-container-status.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+container_name="$1"
+is_health_check_success=0
+
+echo "container name is $container_name"
+for i in 1 2 3 4 5
+do
+  echo "checking status times: $i"
+  status=$(docker inspect --format {{.State.Status}} "$container_name" | head -n 1)
+  echo "the container $container_name status is: $status"
+   if [[ ${status} == "running" ]]; then
+          is_health_check_success=1
+          break
+   fi
+   sleep 1
+   echo "continue check health for $container_name ${i} times..."
+done
+
+
+if [[ ${is_health_check_success} == 1 ]]; then
+        echo "$container_name health check success ✓✓✓"
+    else
+        echo "$container_name health check failed XXX"
+        exit 1
+fi
+
