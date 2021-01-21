f58401a3de90a3785e5135ab9625ec5784810b72
wenjun <wjxu@thoughtworks.com>
2021-01-21 11:03:34 +0800

    [#97][Wenjun][Feat] add logo in header

--- scripts/constants.js
+++ scripts/constants.js
@@ -2,17 +2,19 @@ const path = require("path");
 
 const ENTRY_PATH = path.resolve(__dirname, "../src/App.tsx");
 const OUTPUT_PATH = path.resolve(__dirname, "../dist");
+
 const PUBLIC_HTML_PATH = path.resolve(__dirname, "../public/index.html");
+const FAVICON_PATH = path.resolve(__dirname, "../src/assets/images/logo.png");
 
 const isDev = process.env.NODE_ENV === "dev";
 const isTest = process.env.NODE_ENV === "test";
 const enableAnalyzer = !!process.env.ENABLE_ANALYZER;
 
 module.exports = {
-	SOURCE_CODE_PATH: path.resolve(__dirname, "../src"),
 	ENTRY_PATH,
 	OUTPUT_PATH,
 	PUBLIC_HTML_PATH,
+	FAVICON_PATH,
 	isDev,
 	isTest,
 	enableAnalyzer,
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,16 +1,37 @@
 import { css } from "@emotion/react";
 import React, { FC } from "react";
+import logo from "../assets/images/logo.png";
 
 const headerStyles = css({
 	height: 64,
-	padding: "0 1138px 0 29px",
-	boxShadow: "0 2px 8px 0 #f0f1f2",
+	padding: "8px 24px",
+	boxShadow: "0 2px 4px #f0f1f2",
 	backgroundColor: "#ffffff",
+	display: "flex",
+	alignItems: "center",
 });
-console.log("headerStyle", headerStyles);
+
+const logoStyles = css({
+	height: 32,
+	width: 32,
+});
+
+const logoTextStyles = css({
+	fontsSize: 24,
+	color: "#096DD9",
+	marginLeft: 10,
+});
 
 const Header: FC = () => {
-	return <div css={headerStyles}>Header</div>;
+	return (
+		<div css={headerStyles}>
+			<img src={logo} css={logoStyles} alt="logo" />
+			<span css={logoTextStyles}>
+				<span>4 KEY </span>
+				<span css={css({ fontWeight: "bold" })}>METRICS</span>
+			</span>
+		</div>
+	);
 };
 
 export default Header;
