2321b52d96c4f00fd0fa2e3060b9dfd044564a0e
miyakee <miyakee27@gmail.com>
2021-01-22 16:25:30 +0800

    [57][Tingyu] update param order of verify api

--- src/main/kotlin/fourkeymetrics/resource/ConfigurationResource.kt
+++ src/main/kotlin/fourkeymetrics/resource/ConfigurationResource.kt
@@ -1,10 +1,12 @@
 package fourkeymetrics.resource
 
+import fourkeymetrics.model.Dashboard
+import fourkeymetrics.model.PipelineConfiguration
 import fourkeymetrics.service.ConfigurationService
+import fourkeymetrics.vo.ConfigurationVo
+import fourkeymetrics.vo.PipelineConfigurationVo
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestParam
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
 class ConfigurationResource {
@@ -15,8 +17,8 @@ class ConfigurationResource {
     @GetMapping("/api/pipeline/verify")
     fun verifyPipeline(
         @RequestParam url: String,
-        @RequestParam credential: String,
         @RequestParam username: String,
+        @RequestParam credential: String,
         @RequestParam type: String
     ) {
         configurationService.verifyPipeline(url, username, credential, type)
