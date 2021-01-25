5136052e188251a0be36f72064f4bc88b1efa5de
wenjun <wjxu@thoughtworks.com>
2021-01-25 11:19:34 +0800

    [#105][Wenjun][Feat] add header for config success component

--- src/config/components/ConfigSuccess.tsx
+++ src/config/components/ConfigSuccess.tsx
@@ -1,7 +1,35 @@
+import { Button } from "antd";
 import React, { FC } from "react";
+import Success from "../../assets/icons/Success";
 
 const ConfigSuccess: FC = () => {
-	return <div>Config Finish</div>;
+	return (
+		<div css={{ display: "flex", alignItems: "center" }}>
+			<Success css={{ flexBasis: 85 }} />
+			<div
+				css={{
+					display: "flex",
+					flexDirection: "column",
+					alignItems: "flex-start",
+					fontSize: 24,
+					color: "black",
+					margin: "0 30px",
+				}}>
+				<span>Pipeline successfully created!</span>
+				<span
+					css={{
+						fontSize: 14,
+						opacity: 0.5,
+					}}>
+					The initial configuration is complete，4 Key Metrics will be presented based on the
+					following projects
+				</span>
+			</div>
+			<Button type={"primary"} size={"large"}>
+				Go to Dashboard
+			</Button>
+		</div>
+	);
 };
 
 export default ConfigSuccess;
