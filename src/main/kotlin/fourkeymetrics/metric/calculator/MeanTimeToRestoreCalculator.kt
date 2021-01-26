69daf2eb635c04abd73f0f778fe8368b0bcfe03b
HeZhuConnie <he.zhu@thoughtworks.com>
2021-01-26 15:37:37 +0800

    [52][Yong & He] calculate mttr

--- src/main/kotlin/fourkeymetrics/metric/calculator/MeanTimeToRestoreCalculator.kt
+++ src/main/kotlin/fourkeymetrics/metric/calculator/MeanTimeToRestoreCalculator.kt
@@ -2,14 +2,28 @@ package fourkeymetrics.metric.calculator
 
 import fourkeymetrics.metric.model.*
 import org.springframework.stereotype.Component
+import java.math.RoundingMode
 
 @Component
 class MeanTimeToRestoreCalculator : MetricCalculator {
 
+    companion object {
+        private const val MILLISECOND_TO_HOURS: Double = 3600000.0
+    }
+
     override fun calculateValue(allBuilds: List<Build>, startTimestamp: Long,
                                 endTimestamp: Long, targetStage: String): Double {
         val buildOrderByTimestampAscending = allBuilds.sortedBy { it.timestamp }
-        TODO("Not yet implemented")
+        val selectedStages = findSelectedStages(buildOrderByTimestampAscending,
+            startTimestamp, endTimestamp, targetStage)
+
+        val restoredPairs = generateRestoredPairs(selectedStages)
+
+        val totalTimeToRestore = restoredPairs.map {
+            (it.second.getStageDoneTime().minus(it.first.getStageDoneTime())) / MILLISECOND_TO_HOURS
+        }.sum()
+
+        return totalTimeToRestore.div(restoredPairs.size).toBigDecimal().setScale(2, RoundingMode.HALF_UP).toDouble()
     }
 
     override fun calculateLevel(value: Double, unit: MetricUnit?): LEVEL {
@@ -36,7 +50,7 @@ class MeanTimeToRestoreCalculator : MetricCalculator {
         }
     }
 
-    fun generateRestoredBuildPairs(selectedStages: List<Stage>): List<Pair<Stage, Stage>> {
+    fun generateRestoredPairs(selectedStages: List<Stage>): List<Pair<Stage, Stage>> {
         val restoredStagePairs: MutableList<Pair<Stage, Stage>> = emptyList<Pair<Stage, Stage>>().toMutableList()
         var index = 0
         while (index < selectedStages.size) {
--- src/test/kotlin/fourkeymetrics/metric/calculator/MeanTimeToRestoreCalculatorTest.kt
+++ src/test/kotlin/fourkeymetrics/metric/calculator/MeanTimeToRestoreCalculatorTest.kt
@@ -85,7 +85,7 @@ internal class MeanTimeToRestoreCalculatorTest {
 
         val selectedStages: List<Stage> = selectedBuilds.flatMap { build -> build.stages }.filter { stage -> stage.name == targetStage }
 
-        val restoredStagePairs = meanTimeToRestoreCalculator.generateRestoredBuildPairs(selectedStages)
+        val restoredStagePairs = meanTimeToRestoreCalculator.generateRestoredPairs(selectedStages)
 
         assertThat(restoredStagePairs.size).isEqualTo(2)
         assertThat(restoredStagePairs[0].first.status).isEqualTo(BuildStatus.FAILED)
@@ -108,12 +108,39 @@ internal class MeanTimeToRestoreCalculatorTest {
 
         val selectedStages: List<Stage> = selectedBuilds.flatMap { build -> build.stages }.filter { stage -> stage.name == targetStage }
 
-        val restoredStagePairs = meanTimeToRestoreCalculator.generateRestoredBuildPairs(selectedStages)
+        val restoredStagePairs = meanTimeToRestoreCalculator.generateRestoredPairs(selectedStages)
 
         assertThat(restoredStagePairs.size).isEqualTo(1)
         assertThat(restoredStagePairs[0].first.status).isEqualTo(BuildStatus.FAILED)
         assertThat(restoredStagePairs[0].first.startTimeMillis).isEqualTo(1609344000000L)
         assertThat(restoredStagePairs[0].second.status).isEqualTo(BuildStatus.SUCCESS)
         assertThat(restoredStagePairs[0].second.startTimeMillis).isEqualTo(1609459200000L)
+    }
+
+    /**
+     * test file: builds-for-MTTR-case-1.json
+     * build 1 : deploy to prod, SUCCESS, end at 2020-12-29
+     * build 2 : deploy to prod, FAILED, end at 2020-12-30 *
+     * build 3 : deploy to prod, FAILED, end at 2020-12-31 *
+     * build 4 : deploy to prod, SUCCESS, end at 2021-01-01 *
+     * build 5 : deploy to prod, FAILED, end at 2021-01-02 *
+     * build 6 : deploy to prod, SUCCESS, end at 2021-01-03 *
+     * build 7 : deploy to prod, SUCCESS, end at 2021-01-05
+     */
+    @Test
+    internal fun `should return MTTR given all builds`() {
+        val allBuilds: List<Build> = objectMapper.readValue(
+            this.javaClass.getResource("/calculator/builds-for-MTTR-case-1.json").readText()
+        )
+
+        val startTimestamp = 1609459200000L // 2021.01.01
+        val endTimestamp = 1609689600000L // 2021.01.04
+        val targetStage = "deploy to prod"
+
+        val mttr = meanTimeToRestoreCalculator.calculateValue(allBuilds, startTimestamp, endTimestamp, targetStage)
+
+        // first mttr 56.00
+        // second mttr 24.00
+        assertThat(mttr).isEqualTo(40.00)
     }
 }
