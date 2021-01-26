6383fe6b12665eb64f85c12de9a25e013c2b1add
Piaopiao <kikychn@gmail.com>
2021-01-26 17:45:05 +0800

    [51][Binfang & Piaopiao] remove level field from api when it is null

--- src/main/kotlin/fourkeymetrics/metrics/model/Metrics.kt
+++ src/main/kotlin/fourkeymetrics/metrics/model/Metrics.kt
@@ -1,10 +1,19 @@
 package fourkeymetrics.metrics.model
 
+import com.fasterxml.jackson.annotation.JsonInclude
+
+
 enum class LEVEL {
     ELITE, HIGH, MEDIUM, LOW, INVALID
 }
 
-data class Metrics(val value: Double, val level: LEVEL?, val startTimestamp: Long, val endTimestamp: Long) {
+@JsonInclude(JsonInclude.Include.NON_NULL)
+data class Metrics(
+    val value: Double,
+    val level: LEVEL?,
+    val startTimestamp: Long,
+    val endTimestamp: Long
+) {
     constructor(value: Double, startTimestamp: Long, endTimestamp: Long) : this(
         value,
         null,
