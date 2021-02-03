a26b7c64c305458d60a67ad189363b61e072f3de
RongZhou <rzhou@thoughtworks.com>
2021-02-03 18:25:38 +0800

    [57][Rong & Binfang] add tests for PipelineApplicationServiceTest

---
+++ src/test/kotlin/fourkeymetrics/dashboard/controller/applicationservice/PipelineApplicationServiceTest.kt
@@ -0,0 +1,122 @@
+package fourkeymetrics.dashboard.controller.applicationservice
+
+import fourkeymetrics.dashboard.buildPipelineVerificationRequest
+import fourkeymetrics.dashboard.model.Pipeline
+import fourkeymetrics.dashboard.model.PipelineType
+import fourkeymetrics.dashboard.repository.DashboardRepository
+import fourkeymetrics.dashboard.repository.PipelineRepository
+import fourkeymetrics.dashboard.service.jenkins.JenkinsPipelineService
+import fourkeymetrics.exception.ApplicationException
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.api.extension.ExtendWith
+import org.mockito.InjectMocks
+import org.mockito.Mock
+import org.mockito.Mockito.`when`
+import org.mockito.Mockito.times
+import org.mockito.Mockito.verify
+import org.springframework.http.HttpStatus
+import org.springframework.test.context.junit.jupiter.SpringExtension
+
+@ExtendWith(SpringExtension::class)
+internal class PipelineApplicationServiceTest {
+    @Mock
+    private lateinit var jenkinsPipelineService: JenkinsPipelineService
+
+    @Mock
+    private lateinit var pipelineRepository: PipelineRepository
+
+    @Mock
+    private lateinit var dashboardRepository: DashboardRepository
+
+    @InjectMocks
+    private lateinit var pipelineApplicationService: PipelineApplicationService
+
+    @Test
+    internal fun `should throw ApplicationException when verifyPipeline() called given pipeline type is not JENKINS`() {
+        val pipelineVerificationRequest = buildPipelineVerificationRequest().copy(type = PipelineType.BAMBOO)
+
+        val exception =
+            assertThrows<ApplicationException> { pipelineApplicationService.verifyPipeline(pipelineVerificationRequest) }
+
+        assertEquals("Pipeline type not support", exception.message)
+        assertEquals(HttpStatus.BAD_REQUEST, exception.httpStatus)
+    }
+
+    @Test
+    internal fun `should invoke jenkinsPipelineService to verify when verifyPipeline() called given pipeline type is JENKINS`() {
+        val pipelineVerificationRequest = buildPipelineVerificationRequest().copy(type = PipelineType.JENKINS)
+
+        pipelineApplicationService.verifyPipeline(pipelineVerificationRequest)
+
+        verify(jenkinsPipelineService, times(1)).verifyPipelineConfiguration(
+            pipelineVerificationRequest.url,
+            pipelineVerificationRequest.username,
+            pipelineVerificationRequest.credential
+        )
+    }
+
+    // @Test
+    // internal fun `should return PipelineResponse when updatePipeline() called and successfully`() {
+    //     val pipelineRequest = buildPipelineRequest()
+    //     val dashboardId = "dashboardId"
+    //     val pipelineId = "pipelineId"
+    //     val newPipeline = Pipeline(
+    //         id = pipelineId,
+    //         dashboardId = dashboardId,
+    //         name = pipelineRequest.name,
+    //         username = pipelineRequest.username,
+    //         credential = pipelineRequest.credential,
+    //         url = pipelineRequest.url,
+    //         type = pipelineRequest.type
+    //     )
+    //     `when`(pipelineRepository.save()).thenReturn(newPipeline)
+    //
+    //     val result = pipelineApplicationService.updatePipeline(dashboardId, pipelineId, pipelineRequest)
+    //
+    //     verify(dashboardRepository).findById(dashboardId)
+    //     verify(pipelineRepository).findById(pipelineId)
+    //     verify(pipelineRepository).save(argThat { pipeline ->
+    //         assertEquals(dashboardId, pipeline.dashboardId)
+    //         assertEquals(pipelineId, pipeline.id)
+    //         assertEquals(pipelineRequest.name, pipeline.name)
+    //         assertEquals(pipelineRequest.username, pipeline.username)
+    //         assertEquals(pipelineRequest.credential, pipeline.credential)
+    //         assertEquals(pipelineRequest.url, pipeline.url)
+    //         assertEquals(pipelineRequest.type, pipeline.type)
+    //         true
+    //     })
+    //     assertEquals(pipelineRequest.name, result.name)
+    //     assertEquals(pipelineRequest.url, result.url)
+    //     assertEquals(pipelineRequest.username, result.username)
+    //     assertEquals(pipelineRequest.credential, result.credential)
+    //     assertEquals(pipelineRequest.type, result.type)
+    // }
+
+    @Test
+    internal fun `should return PipelineResponse when getPipeline() called`() {
+        val pipelineId = "pipelineId"
+        val dashboardId = "dashboardId"
+        val pipeline = Pipeline(
+            id = pipelineId,
+            dashboardId = dashboardId,
+            name = "name",
+            username = "username",
+            credential = "credential",
+            url = "url",
+            type = PipelineType.JENKINS
+        )
+        `when`(pipelineRepository.findById(pipelineId)).thenReturn(pipeline)
+
+        val result = pipelineApplicationService.getPipeline(dashboardId, pipelineId)
+
+        verify(dashboardRepository).findById(dashboardId)
+        assertEquals(pipeline.id, result.id)
+        assertEquals(pipeline.name, result.name)
+        assertEquals(pipeline.username, result.username)
+        assertEquals(pipeline.credential, result.credential)
+        assertEquals(pipeline.url, result.url)
+        assertEquals(pipeline.type, result.type)
+    }
+}
