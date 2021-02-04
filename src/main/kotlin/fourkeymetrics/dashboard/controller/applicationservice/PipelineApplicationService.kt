29224ce9134e4f67cc85f3137ea36807e8df661c
RongZhou <rzhou@thoughtworks.com>
2021-02-04 16:01:26 +0800

    [57][Rong & Binfang] remove suppress warning in PipelineApplicationService

--- src/main/kotlin/fourkeymetrics/dashboard/controller/applicationservice/PipelineApplicationService.kt
+++ src/main/kotlin/fourkeymetrics/dashboard/controller/applicationservice/PipelineApplicationService.kt
@@ -13,7 +13,6 @@ import org.bson.types.ObjectId
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 
-@SuppressWarnings("TooManyFunctions")
 @Service
 class PipelineApplicationService {
     @Autowired
