13e5892ecab8a494d863204e6ed17702c307dd46
reeli <reeli.null@gmail.com>
2021-02-05 16:04:37 +0800

    [#99][Rui][fix] change wording

--- src/constants/errorMessages.tsx
+++ src/constants/errorMessages.tsx
@@ -2,7 +2,7 @@ export const ERROR_MESSAGES = {
 	EMPTY_DASHBOARD_NAME: "Please input dashboard name",
 	EMPTY_PIPELINE_NAME: "Please input pipeline name",
 	EMPTY_PIPELINE_TOOL: "Please input pipeline tool",
-	EMPTY_DOMAIN_NAME: "Please input pipeline domain",
+	EMPTY_DOMAIN_NAME: "Please input pipeline url",
 	EMPTY_USERNAME: "Please input username",
-	EMPTY_CREDENTIAL: "Please input credential",
+	EMPTY_CREDENTIAL: "Please input token",
 };
