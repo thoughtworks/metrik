43c3aa59c71fc4239ac8d7f2f6e91434d531e6e3
reeli <reeli.null@gmail.com>
2021-02-07 16:34:48 +0800

    [#136][Rui][feat] verify the dashboard ID before redirecting to landing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,25 @@
-import React, { FC } from "react";
+import React, { FC, useEffect } from "react";
 import ReactDom from "react-dom";
 import { Routes } from "./Routes";
 import { BrowserRouter as Router, Switch } from "react-router-dom";
 import Header from "./components/Header";
+import { getDashboardsUsingGet } from "./clients/apis";
+import { useRequest } from "./hooks/useRequest";
+import { isEmpty } from "lodash";
 
 export const App: FC = () => {
-	return (
+	const [dashboards, getDashboardsRequest] = useRequest(getDashboardsUsingGet, []);
+
+	useEffect(() => {
+		getDashboardsRequest(undefined);
+	}, []);
+
+	console.log(dashboards, "dashboards");
+	return isEmpty(dashboards) ? null : (
 		<Router>
 			<Header />
 			<Switch>
-				<Routes />
+				<Routes dashboardId={dashboards[0]?.id} />
 			</Switch>
 		</Router>
 	);
--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,13 +1,18 @@
 import React from "react";
 import { PageDashboard } from "./dashboard/PageDashboard";
 import { PageConfig } from "./config/PageConfig";
-import { Route } from "react-router-dom";
+import { Redirect, Route } from "react-router-dom";
 
-export const Routes = () => {
-	return (
-		<>
-			<Route path={"/"} component={PageDashboard} exact />
-			<Route path={"/config"} component={PageConfig} exact />
-		</>
-	);
-};
+export const Routes = ({ dashboardId }: { dashboardId: string | undefined }) => (
+	<>
+		<Redirect from={"/config"} to={"/"} exact />
+		<Route path={"/"} exact>
+			{dashboardId ? (
+				<Redirect to={{ pathname: "/dashboard", search: `?dashboardId=${dashboardId}` }} />
+			) : (
+				<PageConfig />
+			)}
+		</Route>
+		<Route path={"/dashboard"} component={PageDashboard} exact />
+	</>
+);
