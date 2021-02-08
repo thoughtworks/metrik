73a3f264c62ef0c0b7a5264f6828fb30edc8752e
binfangHUANG <bfhuang@thoughtworks.com>
2021-02-09 00:18:29 +0800

    [Binfang] add the mongo db transaction manager bean

---
+++ src/main/kotlin/fourkeymetrics/applicationconfig/MongoConfiguration.kt
@@ -0,0 +1,16 @@
+package fourkeymetrics.applicationconfig
+
+//import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+//import org.springframework.data.mongodb.MongoDatabaseFactory
+//import org.springframework.data.mongodb.MongoTransactionManager
+//import org.springframework.transaction.TransactionManager
+
+
+@Configuration
+class MongoConfiguration {
+//    @Bean
+//    fun transactionManager(factory: MongoDatabaseFactory?): TransactionManager? {
+//        return MongoTransactionManager(factory!!)
+//    }
+}
