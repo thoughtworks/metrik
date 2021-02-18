42f2104165581beca041e5f82b376448fb04189c
Piaopiao <kikychn@gmail.com>
2021-02-18 10:31:37 +0800

    [#123][Lan][Feat] update scroll bar for chart

--- src/components/LineChart.tsx
+++ src/components/LineChart.tsx
@@ -1,6 +1,5 @@
-import React, { FC, useState } from "react";
+import React, { FC, useEffect, useRef, useState } from "react";
 import {
-	Brush,
 	CartesianGrid,
 	Line,
 	LineChart as RechartsLineChart,
@@ -9,7 +8,9 @@ import {
 	YAxis,
 } from "recharts";
 import { Metrics } from "../clients/apis";
-import { GRAY_7, GRAY_8 } from "../constants/styles";
+import { GRAY_1, GRAY_7 } from "../constants/styles";
+import { css } from "@emotion/react";
+import { map, throttle } from "lodash";
 
 export interface CustomizeTickProps {
 	x?: number;
@@ -28,88 +29,136 @@ interface LineChartProps {
 }
 
 const domainMaximizeRatio = 1.1;
+const lineUnit = 100;
+const yAxisWidth = 80;
+const fullDisplayAmount = 9;
+const yAxisStyles = css({
+	width: 80,
+	height: 300,
+	position: "absolute",
+	backgroundColor: "#ffffff",
+	zIndex: 1000,
+});
+const heightStyles = css({
+	height: 300,
+});
 
 export const LineChart: FC<LineChartProps> = ({ data, yaxisFormatter, unit, CustomizeTick }) => {
-	const [initDomain, setInitDomain] = useState(false);
-	const [yAxisMax, setYAxisMax] = useState(0);
+	const dataMax = Math.max(...map(data, item => item.value).filter(item => !isNaN(item)));
+	const yMaxValue = dataMax === 0 ? 1 : Math.ceil((dataMax * domainMaximizeRatio) / 10) * 10;
+	const ref = useRef<HTMLDivElement>(null);
+	const [chartWidth, setChartWidth] = useState<string | number>("100%");
+	const scrollWidth = data.length ? lineUnit * (data.length - 1) + yAxisWidth : 0;
+
+	useEffect(() => {
+		if (ref.current) {
+			setChartWidth(data.length > fullDisplayAmount ? ref.current?.offsetWidth : "100%");
+			window.addEventListener(
+				"resize",
+				throttle(() => {
+					() => setChartWidth(ref.current?.offsetWidth || "100%");
+				}, 500)
+			);
+		}
+	}, [ref.current, chartWidth]);
 
 	return (
-		<ResponsiveContainer width="100%" height={300}>
-			<RechartsLineChart
-				css={{
-					".recharts-brush-texts": {
-						fontSize: 0,
-					},
-					".recharts-brush > rect": {
-						stroke: "none",
-						strokeLinecap: "round",
-					},
-				}}
-				data={data}
-				margin={{
-					top: 5,
-					right: 30,
-					left: 20,
-					bottom: 20,
-				}}>
-				<CartesianGrid stroke="#416180" strokeWidth={0.5} strokeOpacity={0.2} vertical={false} />
-				<XAxis
-					dataKey="startTimestamp"
-					stroke="#416180"
-					strokeWidth={0.5}
-					strokeOpacity={0.45}
-					height={50}
-					padding={{ left: 20, right: 20 }}
-					tick={<CustomizeTick data={data} />}
-				/>
-				<YAxis
-					tickFormatter={yaxisFormatter}
-					axisLine={false}
-					label={{ value: unit, angle: -90, position: "insideLeft" }}
-					tickLine={false}
-					domain={
-						!initDomain
-							? [
-									0,
-									(dataMax: number) => {
-										setInitDomain(true);
-										const yAxisMaxValue =
-											dataMax === 0 ? 1 : Math.ceil((dataMax * domainMaximizeRatio) / 10) * 10;
-										setYAxisMax(yAxisMaxValue);
-										return yAxisMaxValue;
-									},
-							  ]
-							: [0, yAxisMax]
-					}
-				/>
-				<Line
-					type="monotone"
-					dataKey="value"
-					stroke={GRAY_7}
-					strokeWidth={2}
-					fill={GRAY_7}
-					isAnimationActive={false}
-					// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-					// @ts-ignore
-					label={{
-						position: "top",
-						formatter: yaxisFormatter,
-						fontSize: 12,
-						style: { transform: "translateY(-5px)" },
-					}}
-				/>
+		<div ref={ref}>
+			<div css={yAxisStyles}>
+				<ResponsiveContainer width="100%" height="80%">
+					<RechartsLineChart
+						data={data}
+						margin={{
+							top: 5,
+							right: 30,
+							left: 20,
+							bottom: 20,
+						}}>
+						<CartesianGrid
+							stroke="#416180"
+							strokeWidth={0.5}
+							strokeOpacity={0.2}
+							vertical={false}
+						/>
+						<XAxis
+							dataKey="startTimestamp"
+							stroke="#416180"
+							strokeWidth={0.5}
+							strokeOpacity={0.45}
+							height={50}
+							padding={{ left: 20, right: 20 }}
+							tick={<CustomizeTick data={data} />}
+						/>
+						<YAxis
+							tickFormatter={yaxisFormatter}
+							axisLine={false}
+							label={{ value: unit, angle: -90, position: "insideLeft" }}
+							tickLine={false}
+							domain={[0, yMaxValue]}
+						/>
+					</RechartsLineChart>
+				</ResponsiveContainer>
+			</div>
 
-				{data.length > 6 && (
-					<Brush
-						height={8}
-						stroke={GRAY_8}
-						startIndex={Math.ceil(data.length / 2)}
-						endIndex={data.length - 1}
-						travellerWidth={0}
-						dataKey="startTimestamp"
-					/>
-				)}
-			</RechartsLineChart>
-		</ResponsiveContainer>
+			<div
+				css={css`
+					width: ${chartWidth};
+					height: 300;
+					position: relative;
+					overflow-x: scroll;
+				`}>
+				<div css={heightStyles}>
+					<ResponsiveContainer width={scrollWidth > chartWidth ? scrollWidth : chartWidth}>
+						<RechartsLineChart
+							data={data}
+							margin={{
+								top: 5,
+								right: 30,
+								left: 20,
+								bottom: 20,
+							}}>
+							<CartesianGrid
+								stroke="#416180"
+								strokeWidth={0.5}
+								strokeOpacity={0.2}
+								vertical={false}
+							/>
+							<XAxis
+								dataKey="startTimestamp"
+								stroke="#416180"
+								strokeWidth={0.5}
+								strokeOpacity={0.45}
+								height={50}
+								padding={{ left: 20, right: 20 }}
+								tick={<CustomizeTick data={data} />}
+							/>
+							<YAxis
+								tickFormatter={yaxisFormatter}
+								axisLine={false}
+								label={{ value: unit, angle: -90, position: "insideLeft" }}
+								tickLine={false}
+								domain={[0, yMaxValue]}
+							/>
+							<Line
+								type="monotone"
+								dataKey="value"
+								stroke={GRAY_7}
+								strokeWidth={2}
+								fill={GRAY_1}
+								isAnimationActive={false}
+								// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+								// @ts-ignore
+								label={{
+									position: "top",
+									formatter: yaxisFormatter,
+									fontSize: 12,
+									style: { transform: "translateY(-5px)" },
+								}}
+							/>
+						</RechartsLineChart>
+					</ResponsiveContainer>
+				</div>
+			</div>
+		</div>
 	);
 };
