e494ca4755db068ab62d897f58a0cad89202fa38
wenjun <wjxu@thoughtworks.com>
2021-02-25 13:54:31 +0800

    [#175][Wenjun][Fix] add favicon

--- scripts/constants.js
+++ scripts/constants.js
@@ -4,6 +4,7 @@ const ENTRY_PATH = path.resolve(__dirname, "../src/App.tsx");
 const OUTPUT_PATH = path.resolve(__dirname, "../dist");
 
 const PUBLIC_HTML_PATH = path.resolve(__dirname, "../public/index.html");
+const FAVICON_PATH = path.resolve(__dirname, "../src/shared/assets/source/logo.svg");
 
 const isDev = process.env.NODE_ENV === "dev";
 const isTest = process.env.NODE_ENV === "test";
@@ -13,6 +14,7 @@ module.exports = {
 	ENTRY_PATH,
 	OUTPUT_PATH,
 	PUBLIC_HTML_PATH,
+	FAVICON_PATH,
 	isDev,
 	isTest,
 	enableAnalyzer,
