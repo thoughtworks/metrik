0809dac0aae505f9d45b408fce7a12a32d92e187
reeli <reeli.null@gmail.com>
2021-02-26 17:34:29 +0800

    [#189][Rui][test] add more tests cases

--- src/shared/utils/__tests__/calcMaxValueWithRatio.test.ts
+++ src/shared/utils/__tests__/calcMaxValueWithRatio.test.ts
@@ -1,8 +1,30 @@
 import { calcMaxValueWithRatio } from "../calcMaxValueWithRatio";
 
 describe("#calcMaxValueWithRatio", () => {
-	it("should return default max value if there is no valid metrics value", () => {
+	it("should return default max value if metrics is empty", () => {
 		expect(calcMaxValueWithRatio([], 1, 1)).toEqual(1);
+	});
+
+	it("should return default max value if there is no valid metrics value", () => {
+		expect(
+			calcMaxValueWithRatio(
+				[
+					{ value: "NaN", startTimestamp: 1610726400000, endTimestamp: 1611935999999 },
+					{ value: "NaN", startTimestamp: 1610726400000, endTimestamp: 1611935999999 },
+				],
+				100,
+				1
+			)
+		).toEqual(100);
+	});
+
+	it("should return default max value 1 if theres is no valid metrics value without a given default value", () => {
+		expect(
+			calcMaxValueWithRatio([
+				{ value: "NaN", startTimestamp: 1610726400000, endTimestamp: 1611935999999 },
+				{ value: "NaN", startTimestamp: 1610726400000, endTimestamp: 1611935999999 },
+			])
+		).toEqual(1);
 	});
 
 	it("should return default max value if metrics max value is 0", () => {
@@ -49,8 +71,17 @@ describe("#calcMaxValueWithRatio", () => {
 					{ value: 100, startTimestamp: 1610726400000, endTimestamp: 1611935999999 },
 				],
 				1,
-				1.1
+				2
 			)
+		).toEqual(1800);
+	});
+
+	it("should return max value with default ratio", () => {
+		expect(
+			calcMaxValueWithRatio([
+				{ value: 899, startTimestamp: 1603727999000, endTimestamp: 1604678399999 },
+				{ value: 100, startTimestamp: 1610726400000, endTimestamp: 1611935999999 },
+			])
 		).toEqual(990);
 	});
 });
