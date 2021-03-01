4e78acff533a7773f086171f8677ee8d7c60b908
reeli <reeli.null@gmail.com>
2021-03-01 15:25:25 +0800

    [#193][Rui][feat] add 'edit' icon beside project title( single click)

--- src/shared/components/EditableText.tsx
+++ src/shared/components/EditableText.tsx
@@ -1,5 +1,7 @@
 import { Input, Typography } from "antd";
 import React, { FC, useState } from "react";
+import { EditOutlined } from "@ant-design/icons";
+import { BLUE_5 } from "../constants/styles";
 
 const { Title } = Typography;
 
@@ -12,33 +14,38 @@ export const EditableText: FC<EditableTextProps> = ({ defaultValue, onEditDone }
 	const [editable, setEditable] = useState(false);
 	const [value, setValue] = useState(defaultValue);
 
+	const handleEdit = (nextValue: string) => {
+		setValue(nextValue);
+		setEditable(false);
+		onEditDone && onEditDone(nextValue);
+	};
+
 	return (
-		<div css={{ height: 45 }}>
+		<div css={{ height: 45, display: "flex" }}>
 			{editable ? (
 				<Input
 					css={{ cursor: "pointer" }}
 					type={"text"}
 					defaultValue={value}
 					autoFocus
-					onBlur={evt => {
-						const nextValue = evt.target.value || defaultValue;
-
-						setValue(nextValue);
-						setEditable(false);
-
-						onEditDone && onEditDone(nextValue);
+					onBlur={evt => handleEdit(evt.target.value || defaultValue)}
+					onKeyDown={e => {
+						if (e.key === "Enter") {
+							handleEdit((e.target as HTMLInputElement).value || defaultValue);
+						}
 					}}
 				/>
 			) : (
-				<span
-					onDoubleClick={() => {
+				<div
+					css={{ display: "flex", alignItems: "baseline", cursor: "pointer" }}
+					onClick={() => {
 						setEditable(true);
-					}}
-					css={{ cursor: "pointer" }}>
+					}}>
 					<Title level={2} style={{ marginBottom: 0 }}>
 						{value}
 					</Title>
-				</span>
+					<EditOutlined style={{ fontSize: 16, color: BLUE_5, marginLeft: 8 }} />
+				</div>
 			)}
 		</div>
 	);
