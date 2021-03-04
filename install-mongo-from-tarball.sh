1ebcdf5bded40cb66c0bfa8f288aa8c1398c3b9e
hjli <hjli@hjlideMacBook-Pro.local>
2021-03-04 21:11:53 +0800

    [172][HJ] add cache for mongo installation

--- install-mongo-from-tarball.sh
+++ install-mongo-from-tarball.sh
@@ -5,3 +5,4 @@ wget https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-debian10-4.4.4.tgz
 tar -zxvf mongodb-linux-*-4.4.4.tgz
 sudo ln -s ${PWD}/mongodb-linux-x86_64-debian10-4.4.4/bin/* /usr/local/bin/
 mongo --eval 'db.serverStatus()'
+echo "success"
