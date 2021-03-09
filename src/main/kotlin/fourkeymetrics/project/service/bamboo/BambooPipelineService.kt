c8d67a6455ca65217cf9ec71b3ecd919f2a6cf75
Hao Yang <xz816111@outlook.com>
2021-03-09 14:56:41 +0800

    [173][Pei & Hao] Implemented Bamboo verification

--- src/main/kotlin/fourkeymetrics/project/service/bamboo/BambooPipelineService.kt
+++ src/main/kotlin/fourkeymetrics/project/service/bamboo/BambooPipelineService.kt
@@ -2,19 +2,36 @@ package fourkeymetrics.project.service.bamboo
 
 import fourkeymetrics.common.model.Build
 import fourkeymetrics.common.model.Status
+import fourkeymetrics.exception.ApplicationException
 import fourkeymetrics.project.model.Pipeline
 import fourkeymetrics.project.service.PipelineService
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.HttpEntity
+import org.springframework.http.HttpHeaders
+import org.springframework.http.HttpMethod
+import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Service
+import org.springframework.web.client.HttpClientErrorException
+import org.springframework.web.client.HttpServerErrorException
 import org.springframework.web.client.RestTemplate
+import org.springframework.web.client.exchange
 
 @Service
-class BambooPipelineService() : PipelineService() {
-    override fun syncBuilds(pipelineId: String): List<Build> {
-        TODO("Not yet implemented")
+class BambooPipelineService(@Autowired private var restTemplate: RestTemplate) : PipelineService() {
+    override fun verifyPipelineConfiguration(pipeline: Pipeline) {
+        val headers = setAuthHeader(pipeline.credential)
+        val entity = HttpEntity<String>(headers)
+        try {
+            restTemplate.exchange<String>(
+                "${pipeline.url}/rest/api/latest/project/", HttpMethod.GET, entity)
+        } catch (ex: HttpServerErrorException) {
+            throw ApplicationException(HttpStatus.SERVICE_UNAVAILABLE, "Verify website unavailable")
+        } catch (ex: HttpClientErrorException) {
+            throw ApplicationException(HttpStatus.BAD_REQUEST, "Verify failed")
+        }
     }
 
-    override fun verifyPipelineConfiguration(pipeline: Pipeline) {
+    override fun syncBuilds(pipelineId: String): List<Build> {
         TODO("Not yet implemented")
     }
 
@@ -26,4 +43,9 @@ class BambooPipelineService() : PipelineService() {
         TODO("Not yet implemented")
     }
 
+    private fun setAuthHeader(credential: String): HttpHeaders {
+        val headers = HttpHeaders()
+        headers.setBearerAuth(credential)
+        return headers
+    }
 }
--- src/test/kotlin/fourkeymetrics/project/service/bamboo/BambooPipelineServiceTest.kt
+++ src/test/kotlin/fourkeymetrics/project/service/bamboo/BambooPipelineServiceTest.kt
@@ -1,5 +1,64 @@
 package fourkeymetrics.project.service.bamboo
 
-internal class BambooPipelineServiceTest {
+import fourkeymetrics.exception.ApplicationException
+import fourkeymetrics.project.model.Pipeline
+import fourkeymetrics.project.repository.BuildRepository
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.client.RestClientTest
+import org.springframework.boot.test.mock.mockito.MockBean
+import org.springframework.context.annotation.Import
+import org.springframework.test.context.junit.jupiter.SpringExtension
+import org.springframework.test.web.client.MockRestServiceServer
+import org.springframework.test.web.client.match.MockRestRequestMatchers
+import org.springframework.test.web.client.response.MockRestResponseCreators
+import org.springframework.web.client.RestTemplate
 
+
+@ExtendWith(SpringExtension::class)
+@Import(BambooPipelineService::class, RestTemplate::class)
+@RestClientTest
+internal class BambooPipelineServiceTest {
+    @Autowired
+    private lateinit var restTemplate: RestTemplate
+
+    @Autowired
+    private lateinit var bambooPipelineService: BambooPipelineService
+
+    @MockBean
+    private lateinit var buildRepository: BuildRepository
+
+    @Test
+    internal fun `should throw exception when verify pipeline given response is 500`() {
+        val credential = "fake-credential"
+        val baseUrl = "http://localhost"
+        val mockServer = MockRestServiceServer.bindTo(restTemplate).ignoreExpectOrder(true).build()
+
+        mockServer.expect(MockRestRequestMatchers.requestTo("${baseUrl}/rest/api/latest/project/"))
+            .andRespond(
+                MockRestResponseCreators.withServerError()
+            )
+
+        Assertions.assertThrows(ApplicationException::class.java) {
+            bambooPipelineService.verifyPipelineConfiguration(Pipeline(credential = credential, url = baseUrl))
+        }
+    }
+
+    @Test
+    internal fun `should throw exception when verify pipeline given response is 400`() {
+        val credential = "fake-credential"
+        val baseUrl = "http://localhost"
+        val mockServer = MockRestServiceServer.bindTo(restTemplate).ignoreExpectOrder(true).build()
+
+        mockServer.expect(MockRestRequestMatchers.requestTo("${baseUrl}/rest/api/latest/project/"))
+            .andRespond(
+                MockRestResponseCreators.withBadRequest()
+            )
+
+        Assertions.assertThrows(ApplicationException::class.java) {
+            bambooPipelineService.verifyPipelineConfiguration(Pipeline(credential = credential, url = baseUrl))
+        }
+    }
 }
