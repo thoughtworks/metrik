c0e832558551e6b1f8df186ab798754690d05e23
Zhe ZHAO <zhezhao@thoughtworks.com>
2021-03-10 15:50:52 +0800

    [#168][Zhe] Test since issue occurs in EZLink

--- config/mongo/mongo-init.sh
+++ config/mongo/mongo-init.sh
@@ -15,7 +15,7 @@ echo "Stop checking mongod readiness..."
 
 echo "Start mongodb initialization"
 echo "Step 1: initializing replica set"
-sleep 8 && mongo -u $MONGO_INITDB_ROOT_USERNAME -p $MONGO_INITDB_ROOT_PASSWORD < /app/mongo/mongo-init-replica-set.js
+sleep 20 && mongo -u $MONGO_INITDB_ROOT_USERNAME -p $MONGO_INITDB_ROOT_PASSWORD < /app/mongo/mongo-init-replica-set.js
 echo "Step 2: add user"
 sleep 2 && mongo -u $MONGO_INITDB_ROOT_USERNAME -p $MONGO_INITDB_ROOT_PASSWORD < /app/mongo/mongo-create-user.js
 echo "Mongo initilazation done"
