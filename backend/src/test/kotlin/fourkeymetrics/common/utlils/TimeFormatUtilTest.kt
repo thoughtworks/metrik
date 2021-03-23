b982d7eeeeaba3c065c2361bbbc559bfb4769995
Hao Yang <xz816111@outlook.com>
2021-03-23 11:45:56 +0800

    [TECH][Hao] Fixed build

--- backend/src/test/kotlin/fourkeymetrics/common/utlils/TimeFormatUtilTest.kt
+++ backend/src/test/kotlin/fourkeymetrics/common/utlils/TimeFormatUtilTest.kt
@@ -9,7 +9,7 @@ import java.time.ZonedDateTime
 internal class TimeFormatUtilTest {
     @Test
     internal fun `should convert zonedDateTime to timestamp`() {
-        val date: ZonedDateTime = ZonedDateTime.of(2020, 1, 1, 0, 0, 0, 0, ZoneId.of("UTC"))
+        val date: ZonedDateTime = ZonedDateTime.of(2018, 1, 1, 0, 0, 0, 0, ZoneId.of("UTC"))
         assertEquals(1514764800000, mapDateToTimeStamp(date))
     }
 }
