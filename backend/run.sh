434b05ac833bd83e4270992647e75a3b852e5f15
Hao Yang <xz816111@outlook.com>
2021-03-29 17:16:45 +0800

    [TECH][Hao] Use release profile and use environment variables to define sensitive information, so user could replase it when using docker run

--- backend/run.sh
+++ backend/run.sh
@@ -6,4 +6,8 @@ JAR=/app/metrik-backend.jar
 
 java ${JAVA_OPTS} -jar -Dspring.profiles.active=$APP_ENV \
                   -Duser.timezone=Asia/Shanghai \
+                  -DDB_USER=$DB_USER \
+                  -DDB_PASSWORD=$DB_PASSWORD \
+                  -DAES_KEY=$AES_KEY \
+                  -DAES_IV=$AES_IV \
                   "${JAR}"
