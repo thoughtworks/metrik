d311119af6cb175105685b43e7942e0f659454e0
Hao Yang <xz816111@outlook.com>
2021-04-01 17:05:44 +0800

    debug: revert debug code

--- backend/src/main/kotlin/fourkeymetrics/project/controller/SynchronizationController.kt
+++ backend/src/main/kotlin/fourkeymetrics/project/controller/SynchronizationController.kt
@@ -1,7 +1,6 @@
 package fourkeymetrics.project.controller
 
 import fourkeymetrics.project.controller.applicationservice.SynchronizationApplicationService
-import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
@@ -16,15 +15,11 @@ data class SynchronizationRecordResponse(val synchronizationTimestamp: Long?)
 class SynchronizationController {
     @Autowired
     private lateinit var synchronizationApplicationService: SynchronizationApplicationService
-    private var logger = LoggerFactory.getLogger(this.javaClass.name)
 
     @PostMapping("/api/project/{projectId}/synchronization")
     fun updateBuilds(@PathVariable projectId: String): ResponseEntity<SynchronizationRecordResponse> {
-        logger.info("************** Syncing **************")
-        Thread.sleep(1000 * 60 * 6)
         val updatedTimestamp = synchronizationApplicationService.synchronize(projectId)
 
-        logger.info("************** done **************")
         return if (updatedTimestamp == null) {
             ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build()
         } else {
