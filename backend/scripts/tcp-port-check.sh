f6a411dd7938ab5ff69a7662fd9128b22dbadb81
Hackl0us <hello@hack0lus.com>
2021-04-13 17:33:31 +0800

    update(ci): Correct shell syntax.

--- backend/scripts/tcp-port-check.sh
+++ backend/scripts/tcp-port-check.sh
@@ -1,9 +1,9 @@
+#!/bin/bash
+
 ## This script is for checking if the specific TCP port is available on HOST.
 ## arg1: Host / IP
 ## arg2: Port number you would like to check
 ## arg3: Tolerant time (second)
-
-#!/bin/bash
 
 HOSTNAME="$1"
 PORT="$2"
@@ -11,19 +11,19 @@ TOLERANCE="$3"
 
 COUNTER=0
 
-nc -zv $HOSTNAME $PORT 2>&1 >/dev/null
+nc -zv "$HOSTNAME" "$PORT" 2>&1 >/dev/null
 
-while [ $? -ne 0 ] && [ "$COUNTER" -lt "$TOLERANCE" ] ; do
-   sleep 2
-   let COUNTER+=2
-   echo "[$HOSTNAME:$PORT] is down... Waiting for retry (${COUNTER} seconds so far)"
-   nc -zv $HOSTNAME $PORT 2>&1 >/dev/null
+while [ $? -ne 0 ] && [ "$COUNTER" -lt "$TOLERANCE" ]; do
+  sleep 2
+  ((COUNTER += 2))
+  echo "[$HOSTNAME:$PORT] is down... Waiting for retry (${COUNTER} seconds so far)"
+  nc -zv "$HOSTNAME" "$PORT" 2>&1 >/dev/null
 done
 
-if [ $? -eq 0 ] ; then
+if [ $? -eq 0 ]; then
   echo "[$HOSTNAME:$PORT] is up!"
   exit 0
-elif [ "$COUNTER" -ge "$TOLERANCE" ] ; then
+elif [ "$COUNTER" -ge "$TOLERANCE" ]; then
   echo "[$HOSTNAME:$PORT] is not accessible, the service is not running."
   exit 1
 fi
