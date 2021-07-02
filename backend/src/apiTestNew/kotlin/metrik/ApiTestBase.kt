50e3eedef7378bc3f4c744b32229a76f2e10025c
Zhe ZHAO <zhaozhe91.bupt@gmail.com>
2021-07-03 01:09:28 +0800

    feat(backend): Add ProjectApiTest suite

--- backend/src/apiTestNew/kotlin/metrik/ApiTestBase.kt
+++ backend/src/apiTestNew/kotlin/metrik/ApiTestBase.kt
@@ -2,6 +2,8 @@ package metrik
 
 import io.restassured.RestAssured
 import metrik.config.ApiTestConfiguration
+import metrik.project.domain.model.Pipeline
+import metrik.project.domain.model.PipelineType
 import metrik.project.domain.model.Project
 import org.junit.jupiter.api.BeforeEach
 import org.springframework.beans.factory.annotation.Autowired
@@ -27,10 +29,18 @@ internal class ApiTestBase {
     fun setUp() {
         RestAssured.port = springTestRandomServerPort
 
-        val projectName = "project name"
-        val projectId = "project id"
-        val synchronisationTime = 98708L
-        val project = Project(projectId, projectName, synchronisationTime)
+        val project = Project("601cbae825c1392117aa0429", "4-key", 1580709600000L)
         mongoTemplate.save(project)
+
+        val pipeline = Pipeline(
+            "601cbb3425c1392117aa053b",
+            "601cbae825c1392117aa0429",
+            "dfservice",
+            "E9fnMY3UGE6Oms35JzLGgQ==",
+            "FVSR/5o1BYh6dJQBeQaNvQ==",
+            "http://localhost:8001/job/4km-df/",
+            PipelineType.JENKINS
+        )
+        mongoTemplate.save(pipeline)
     }
 }
---
+++ backend/src/apiTestNew/kotlin/metrik/ProjectApiTest.kt
@@ -0,0 +1,45 @@
+package metrik
+
+import io.restassured.RestAssured
+import io.restassured.http.ContentType
+import org.hamcrest.CoreMatchers.`is`
+import org.junit.jupiter.api.Test
+
+internal class ProjectApiTest : ApiTestBase() {
+    @Test
+    fun `should get project list returned`() {
+        RestAssured
+            .given()
+            .contentType(ContentType.JSON)
+            .get("/api/project/")
+            .then()
+            .statusCode(200)
+            .body("size()", `is`(1))
+            .body("[0].id", `is`("601cbae825c1392117aa0429"))
+            .body("[0].name", `is`("4-key"))
+            .body("[0].synchronizationTimestamp", `is`(1580709600000))
+    }
+
+    @Test
+    fun `should get project via project ID`() {
+        RestAssured
+            .given()
+            .contentType(ContentType.JSON)
+            .get("/api/project/601cbae825c1392117aa0429")
+            .then()
+            .statusCode(200)
+            .body("id", `is`("601cbae825c1392117aa0429"))
+            .body("name", `is`("4-key"))
+            .body("synchronizationTimestamp", `is`(1580709600000))
+    }
+
+    @Test
+    fun `should get 404 error returned given an invalid project ID`() {
+        RestAssured
+            .given()
+            .contentType(ContentType.JSON)
+            .get("/api/project/some-invalid-id")
+            .then()
+            .statusCode(404)
+    }
+}
