3eeb9a3224cbafb6ea3c5ebf71f0d7e920dafa9b
Zhe ZHAO <zhaozhe91.bupt@gmail.com>
2021-07-05 01:32:35 +0800

    feat(ci): Use updated gradle build task to run all tests all together. No more mongo container setup required.

--- backend/mongodb-setup/mongodb-for-apitest/remove-mongodb.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-export COMPOSE_PROJECT_NAME=mongodb-apitest
-docker-compose -f docker-compose-for-apitest.yml down
-docker stop mongodb-for-apitest
-docker rm mongodb-for-apitest
-rm -rf mongo-db-data
