4b838fa1d2109cdd6ea2b17aef786367146276fe
Lixing <lixing.sun@thoughtworks.com>
2022-06-06 22:21:32 +0800

    fix(frontend): recover and fix the chart horizontal scrollbar

--- frontend/src/components/LineChart.tsx
+++ frontend/src/components/LineChart.tsx
@@ -32,17 +32,25 @@ interface LineChartProps {
 	yAxisDomain?: AxisDomain;
 }
 
+const lineUnit = 100;
+const chartHeight = 300;
 const yAxisWidth = 72;
+const barWidth = 16;
+const yAxisScrollablePercentageHeight = "78%";
+const yAxisUnscrollablePercentageHeight = "83%";
+const minLengthForDisplayScrollBar = 10;
 
-const chartContainerStyle = css({
-	position: "relative",
-	height: 300,
-	overflowX: "visible",
-});
+const chartContainerStyle = (scrollable: boolean) =>
+	css({
+		position: "relative",
+		height: chartHeight,
+		overflowX: scrollable ? "auto" : "visible",
+		overflowY: scrollable ? "hidden" : "visible",
+	});
 
 const yAxisStyles = css({
 	width: yAxisWidth,
-	height: 300,
+	height: chartHeight - barWidth,
 	position: "absolute",
 	backgroundColor: "#ffffff",
 	zIndex: 1000,
@@ -77,11 +85,38 @@ export const LineChart: FC<LineChartProps> = ({
 	yAxisDomain,
 }) => {
 	const ref = useRef<HTMLDivElement>(null);
+	const scrollWidth = data.length ? lineUnit * (data.length - 1) + yAxisWidth : 0;
+	const [xAxisInterval, setXAxisInterval] = useState<"preserveEnd" | 0>(0);
+
+	useEffect(() => {
+		const adjustXAxisInterval = throttle(() => {
+			setXAxisInterval(
+				data.length < minLengthForDisplayScrollBar &&
+					(ref.current?.offsetWidth === undefined || ref.current?.offsetWidth / data.length <= 80)
+					? "preserveEnd"
+					: 0
+			);
+		}, 500);
+
+		adjustXAxisInterval();
+		window.addEventListener("resize", adjustXAxisInterval);
+
+		return () => {
+			window.removeEventListener("resize", adjustXAxisInterval);
+		};
+	}, []);
 
 	return (
 		<div ref={ref}>
 			<div css={yAxisStyles}>
-				<ResponsiveContainer width="100%" height="80%" id="levelSvg">
+				<ResponsiveContainer
+					width="100%"
+					height={
+						data.length >= minLengthForDisplayScrollBar
+							? yAxisScrollablePercentageHeight
+							: yAxisUnscrollablePercentageHeight
+					}
+					id={"levelSvg"}>
 					<RechartsLineChart
 						margin={{
 							top: 20,
@@ -100,8 +135,10 @@ export const LineChart: FC<LineChartProps> = ({
 				</ResponsiveContainer>
 			</div>
 
-			<div css={chartContainerStyle}>
-				<ResponsiveContainer width="100%" height="80%" id="chartSvg">
+			<div css={chartContainerStyle(data.length >= minLengthForDisplayScrollBar)}>
+				<ResponsiveContainer
+					id={"chartSvg"}
+					width={data.length >= minLengthForDisplayScrollBar ? scrollWidth : "100%"}>
 					<RechartsLineChart
 						data={data}
 						margin={{
@@ -117,7 +154,7 @@ export const LineChart: FC<LineChartProps> = ({
 							vertical={false}
 						/>
 						<XAxis
-							interval={0}
+							interval={xAxisInterval}
 							dataKey="startTimestamp"
 							stroke="#416180"
 							strokeWidth={0.5}
