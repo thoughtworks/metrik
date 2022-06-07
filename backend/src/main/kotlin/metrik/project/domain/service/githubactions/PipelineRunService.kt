2c7956a2b99bab1dea6cbe11e737b50231e323a9
yuan.gu <yuan.gu@thoughtworks.com>
2022-06-07 15:32:57 +0800

    fix: fix GithubPipelineServiceTest and add branchesServiceTest and PipelineCommitServiceTest

---
+++ backend/src/main/kotlin/metrik/project/domain/service/githubactions/PipelineRunService.kt
@@ -0,0 +1,42 @@
+package metrik.project.domain.service.githubactions
+
+import metrik.project.domain.model.PipelineConfiguration
+import metrik.project.domain.repository.BuildRepository
+import org.springframework.stereotype.Service
+import kotlin.streams.toList
+
+@Service("githubActionsPipelineRunService")
+class PipelineRunService(
+    private val buildRepository:BuildRepository,
+    private val runService:RunService,
+    private val githubBranchService: GithubBranchService
+) {
+    fun getNewRuns(pipeline:PipelineConfiguration): MutableList<GithubActionsRun> {
+        val latestTimestamp = getLatestBuildTimeStamp(pipeline)
+        var newRuns = runService.syncRunsByPage(pipeline, latestTimestamp)
+        val branches = githubBranchService.retrieveBranches(pipeline)
+        return newRuns.filter { branches.contains(it.branch) } as MutableList<GithubActionsRun>
+    }
+
+    fun getInProgressRuns(
+        pipeline: PipelineConfiguration,
+        newRuns: MutableList<GithubActionsRun>
+    ) {
+        val inProgressRuns = buildRepository.getInProgressBuilds(pipeline.id)
+            .parallelStream()
+            .map { runService.syncSingleRun(pipeline, it.url) }
+            .toList()
+            .filterNotNull()
+        newRuns.addAll(inProgressRuns)
+    }
+
+    private fun getLatestBuildTimeStamp(pipeline: PipelineConfiguration): Long {
+        val latestTimestamp = when (
+            val latestBuild = buildRepository.getLatestBuild(pipeline.id)
+        ) {
+            null -> Long.MIN_VALUE
+            else -> latestBuild.timestamp
+        }
+        return latestTimestamp
+    }
+}
