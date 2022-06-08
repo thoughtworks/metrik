29b7be5eaad1f023717f9d6dcabcae1b71da09bd
yuan.gu <yuan.gu@thoughtworks.com>
2022-06-08 16:26:37 +0800

    fix: remove default value for BranchResponse

--- backend/src/main/kotlin/metrik/project/infrastructure/github/feign/response/BranchResponse.kt
+++ backend/src/main/kotlin/metrik/project/infrastructure/github/feign/response/BranchResponse.kt
@@ -3,6 +3,6 @@ package metrik.project.infrastructure.github.feign.response;
 import org.apache.logging.log4j.util.Strings
 
 data class BranchResponse(
-    val name: String = Strings.EMPTY,
+    val name: String
 )
 
